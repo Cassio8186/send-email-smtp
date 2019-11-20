964f3f256c50a08f5abbc2e498a3357cf1d3db5a
cassio8186 <cassio8186@gmail.com>
2019-11-19 23:52:51 -0300

    refactor authentication to middlewares

---
+++ src/middleware/auth.js
@@ -0,0 +1,12 @@
+const auth = (req, res, next) => {
+	//TODO put decode user into a middleware
+	if (!req.headers.authorization) {
+		return res.status(401).send({ Error: "Please, authenticate" });
+	}
+	const authDecoded = req.headers.authorization.replace("Basic ", "");
+	const auth = Buffer.from(authDecoded, "base64").toString("ascii");
+	req.auth = auth.split(":");
+	next();
+};
+
+module.exports = auth;
